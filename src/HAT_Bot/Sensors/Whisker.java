7ba05edf2935fc39ce3eb946a300eab0b16bc6e9
KasperAdan <kh.adan@student.avans.nl>
2019-11-14 13:06:26 +0100

    Kasper

---
+++ src/HAT_Bot/Sensors/Whisker.java
@@ -0,0 +1,23 @@
+package HAT_Bot.Sensors;
+
+import TI.BoeBot;
+
+public class Whisker implements Sensor{
+    private int pin;
+
+    public Whisker(int pin) {
+        this.pin = pin;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public int getValue(){
+        if(BoeBot.digitalRead(this.pin)){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
