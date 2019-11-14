24ceb8672061892b90bda2ca1e75723f2429423a
TwanHogervorst <twanhog@gmail.com>
2019-11-14 16:28:08 +0100

    StoppableTimer toegevoegd

---
+++ src/HAT_Bot/Logic/StoppableTimer.java
@@ -0,0 +1,35 @@
+package HAT_Bot.Logic;
+
+import TI.Timer;
+
+public class StoppableTimer extends Timer {
+
+    private boolean stopped;
+
+    public StoppableTimer(int interval){
+        super(interval);
+    }
+
+    public void start(){
+        super.mark();
+        this.stopped = false;
+    }
+
+    public void stop(){
+        this.stopped = true;
+    }
+
+    public boolean isStopped(){
+        return this.stopped;
+    }
+
+    @Override
+    public boolean timeout() {
+        if(this.stopped){
+            return false;
+        }
+        else{
+            return super.timeout();
+        }
+    }
+}
