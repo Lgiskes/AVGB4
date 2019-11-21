ae3f5476ae42d716cb07b96d09f672429b5d35ba
Lgiskes <lars.giskes@gmail.com>
2019-11-21 15:33:57 +0100

    MotionController en TestKlass verbetering

--- src/HAT_Bot/Actuators/Motor.java
+++ src/HAT_Bot/Actuators/Motor.java
@@ -1,24 +1,44 @@
 package HAT_Bot.Actuators;
 
+import HAT_Bot.Logic.Updatable;
 import TI.Servo;
 import TI.Timer;
 
 public class Motor implements Updatable {
 
-
+    private int pin;
+    private boolean inverted;
     private Servo servo;
     private Timer timer;
 
-    public  Motor(int pin) {
-        this.servo = new Servo(pin);
+    public  Motor(int pin, boolean inverted) {
+        this.pin = pin;
+        this.inverted = inverted;
         this.timer = new Timer(10);
+        this.servo = new Servo(pin);
+    }
+
+    public int getSpeed() {
+        int speed = (this.servo.getPulseWidth() - 1500) / 2;
+
+        if(this.inverted){
+            speed *= -1;
+        }
+
+        return speed;
     }
 
     public void setSpeed(int speed) {
-        int speedNew = speed*2+1500;
+        speed = Math.max(-100, speed);
+        speed = Math.min(100, speed);
+
+        if(this.inverted){
+            speed *= -1;
+        }
+
+        int newSpeed = speed * 2 + 1500;
+        this.servo.update(newSpeed);
     }
-
-
 
     @Override
     public void update() {
