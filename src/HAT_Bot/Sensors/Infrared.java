b2a92a3571ef2743a0021c31eb870d5e805fc37a
KasperAdan <kh.adan@student.avans.nl>
2019-11-22 08:32:52 +0100

    IR Fix

--- src/HAT_Bot/Sensors/Infrared.java
+++ src/HAT_Bot/Sensors/Infrared.java
@@ -1,41 +1,23 @@
 package HAT_Bot.Sensors;
 
+import HAT_Bot.Logic.StoppableTimer;
 import HAT_Bot.Logic.Updatable;
 import TI.BoeBot;
 
 public class Infrared implements Sensor, Updatable {
     private int pin;
     private int value;
+    private StoppableTimer timerinfrared;
 
     public Infrared(int pin) {
         this.pin = pin;
+        timerinfrared = new StoppableTimer(10);
     }
 
     public int getValue() {
-        int pulse = BoeBot.pulseIn(this.pin, false, 6000);
-        int bitNumber = 0;
-        if (pulse > 2000) {
-            int lengtes[] = new int[12];
-            for (int i = 0; i < 12; i++) {
-                lengtes[i] = BoeBot.pulseIn(this.pin, false, 20000);
-            }
-            for (int i = lengtes.length-1; i >= 0; i--) {
-                if (lengtes[i] > 200 && lengtes[i] < 850) {
-                    bitNumber += 0;
-                    bitNumber = bitNumber << 1;
-                }
-                else if
-                (lengtes[i] > 950 && lengtes[i] < 1450) {
-                    bitNumber += 1;
-                    bitNumber = bitNumber << 1;
-                }
-                else {
-                    return -1;
-                }
-            }
-        }
-        return bitNumber;
+        return this.value;
     }
+
 
     public int getPin() {
         return this.pin;
@@ -48,6 +30,34 @@ public class Infrared implements Sensor, Updatable {
 
     @Override
     public void update() {
-        getValue();
+        if (timerinfrared.timeout()) {
+            boolean validValue = true;
+            int pulse = BoeBot.pulseIn(this.pin, false, 6000);
+            int bitNumber = 0;
+            if (pulse > 2000) {
+                int lengtes[] = new int[12];
+                for (int i = 0; i < 12; i++) {
+                    lengtes[i] = BoeBot.pulseIn(this.pin, false, 20000);
+                }
+                for (int i = lengtes.length - 1; i >= 0; i--) {
+                    if (lengtes[i] > 200 && lengtes[i] < 850) {
+                        bitNumber += 0;
+                        bitNumber = bitNumber << 1;
+                    } else if
+                    (lengtes[i] > 950 && lengtes[i] < 1450) {
+                        bitNumber += 1;
+                        bitNumber = bitNumber << 1;
+                    } else {
+                        validValue = false;
+                    }
+                }
+            }
+            if (validValue) {
+                this.value = bitNumber;
+            }
+            else {
+                this.value = -1;
+            }
+        }
     }
 }
