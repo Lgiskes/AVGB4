5838747fd76ea554eade58fd950da2fc45c24d3c
LindeHuijben <la.huijben@student.avans>
2019-11-28 16:38:18 +0100

    remote control actuator commends

--- src/HAT_Bot/Controllers/RemoteControlObserver.java
+++ src/HAT_Bot/Controllers/RemoteControlObserver.java
@@ -1,5 +1,10 @@
 package HAT_Bot.Controllers;
 
+/**
+ * Rules for al the remote control classes.
+ */
 public interface RemoteControlObserver {
-    void onRemoteControlDetected(RemoteControl r, RemoteControlCommand command);
+
+    void onRemoteControlDetected(RemoteControl r, RemoteControlCommand command); // looks for the right button and the right command.
+
 }
